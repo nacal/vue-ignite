d0158e04f4e3171a64d3153e17d3c784873c6ca3
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-03-27 23:04:59 +0900

    Add ignite UI packages

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { ModuleManager } from "igniteui-webcomponents-core";
+import { IgcDataGridModule } from "igniteui-webcomponents-grids";
 
 Vue.config.productionTip = false;
+
+ModuleManager.register(IgcDataGridModule);
+
+Vue.config.ignoredElements = ["igc-data-grid"];
 
 new Vue({
   render: (h) => h(App),
