70a7995d0f81d14ef4398f2dc925f9c6d53ca11b
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-03-27 23:31:30 +0900

    Add ignite UI charts

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import { ModuleManager } from "igniteui-webcomponents-core";
 import { IgcDataGridModule } from "igniteui-webcomponents-grids";
+import { IgcCategoryChartModule } from "igniteui-webcomponents-charts";
 
 Vue.config.productionTip = false;
 
-ModuleManager.register(IgcDataGridModule);
+ModuleManager.register(IgcDataGridModule, IgcCategoryChartModule);
 
-Vue.config.ignoredElements = ["igc-data-grid"];
+Vue.config.ignoredElements = ["igc-data-grid", "igc-category-chart"];
 
 new Vue({
   render: (h) => h(App),
