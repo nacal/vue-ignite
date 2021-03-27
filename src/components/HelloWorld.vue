154186cc62c94a0bffa3d018dbd7f10d42389143
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-03-27 23:24:24 +0900

    Develop igc-data-grid

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,122 +1,90 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-  </div>
+  <igc-data-grid
+    height="100%"
+    width="100%"
+    auto-generate-columns="true"
+    ref="grid"
+  >
+  </igc-data-grid>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { Component, Vue } from "vue-property-decorator";
+import { IgcDataGridComponent } from "igniteui-webcomponents-grids";
 
 @Component
 export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+  private dataSource!: Array<{
+    Discontinued: boolean;
+    OrderDate: Date;
+    ProductID: number;
+    ProductName: string;
+    QuantityPerUnit: string;
+    ReorderLevel: number;
+    UnitPrice: number;
+    UnitsInStock: number;
+  }>;
+
+  constructor() {
+    super();
+    this.dataSource = [
+      {
+        Discontinued: false,
+        OrderDate: new Date("2012-02-12"),
+        ProductID: 1,
+        ProductName: "Chai",
+        QuantityPerUnit: "10 boxes x 20 bags",
+        ReorderLevel: 10,
+        UnitPrice: 18.0,
+        UnitsInStock: 39,
+      },
+      {
+        Discontinued: false,
+        OrderDate: new Date("2003-03-17"),
+        ProductID: 2,
+        ProductName: "Chang",
+        QuantityPerUnit: "24 - 12 oz bottles",
+        ReorderLevel: 25,
+        UnitPrice: 19.0,
+        UnitsInStock: 17,
+      },
+      {
+        Discontinued: false,
+        OrderDate: new Date("2006-03-17"),
+        ProductID: 3,
+        ProductName: "Aniseed Syrup",
+        QuantityPerUnit: "12 - 550 ml bottles",
+        ReorderLevel: 25,
+        UnitPrice: 10.0,
+        UnitsInStock: 13,
+      },
+      {
+        Discontinued: false,
+        OrderDate: new Date("2016-03-17"),
+        ProductID: 4,
+        ProductName: "Chef Antony Cajun Seasoning",
+        QuantityPerUnit: "48 - 6 oz jars",
+        ReorderLevel: 0,
+        UnitPrice: 22.0,
+        UnitsInStock: 53,
+      },
+      {
+        Discontinued: true,
+        OrderDate: new Date("2011-11-11"),
+        ProductID: 5,
+        ProductName: "Chef Antony Gumbo Mix",
+        QuantityPerUnit: "36 boxes",
+        ReorderLevel: 0,
+        UnitPrice: 21.35,
+        UnitsInStock: 0,
+      },
+    ];
+  }
+
+  mounted(): void {
+    const igcGrid = this.$refs.grid as IgcDataGridComponent;
+    console.log(igcGrid);
+    igcGrid.dataSource = this.dataSource;
+  }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
